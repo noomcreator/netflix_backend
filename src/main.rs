c3c916682a3fcdfa3f26d0a48e7adf3bfab7ffa1
noomcreator <rueangyottreemat@gmail.com>
2026-01-15 16:53:01 +0700

    chore: add main.rs and finish netflix backend

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use dotenv::dotenv;
+use std::env;
+use tokio::net::TcpListener;
+use tower_http::{cors::CorsLayer, services::ServeDir};
+
+// Modules
+mod handlers;
+mod models;
+mod state;
+
+use state::AppState;
+use handlers::{root, get_trending_movives, search_content, get_movie_videos};
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let api_key = env::var("TMDB_API_KEY").expect("TMDB_API_KEY must be set in .env");
+
+    let state = AppState {
+        tmdb_api_key: api_key,
+        client: reqwest::Client::new(),
+    };
+
+    let cors = CorsLayer::new().allow_origin(tower_http::cors::Any);
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/api/trending", get(get_trending_movives))
+        .route("/api/search", get(search_content))
+        .route("/api/movie/{id}/videos", get(get_movie_videos))
+        .nest_service("stream", ServeDir::new("assets"))
+        .layer(cors)
+        .with_state(state);
+
+    // let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    println!("Server listening on http://{}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
 }
